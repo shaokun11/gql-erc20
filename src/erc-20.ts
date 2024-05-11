55d97c3a57d2190ea6d55328ca2fd0eb14ad9282
shaokun <605416017@qq.com>
2024-05-11 23:44:29 +0800

    fixed

--- src/erc-20.ts
+++ src/erc-20.ts
@@ -1,4 +1,4 @@
-import { Address } from "@graphprotocol/graph-ts"
+import { Address, BigInt, log } from "@graphprotocol/graph-ts"
 import {
   Approval as ApprovalEvent,
   ERC20,
@@ -6,7 +6,7 @@ import {
 } from "../generated/ERC20/ERC20"
 import { Approval, Transfer } from "../generated/schema"
 
-let Token = ERC20.bind(Address.fromString("0x4aCD8A003d7C6d1B02C7F0B44976CcbFC1d07B3f"))
+let Token = ERC20.bind(Address.fromString("0xb130e16D79C853313672B14e54baFe622db22bB3"))
 
 export function handleTransfer(event: TransferEvent): void {
   let entity = new Transfer(
@@ -19,7 +19,7 @@ export function handleTransfer(event: TransferEvent): void {
   entity.blockNumber = event.block.number
   entity.blockTimestamp = event.block.timestamp
   entity.transactionHash = event.transaction.hash
-  entity.balanceFrom = Token.balanceOf(event.params.from)
-
+  entity.balanceFrom = BigInt.fromString("1")
+  log.info("-----Transfer event----------: {}", [entity.id.toHexString()])
   entity.save()
 }
